3cdbb381e8c4a633465f120bd367c9fe0d914c64
Mohammad Satrio <wolfaeon@gmail.com>
2019-11-12 19:50:49 +0700

    Debug

--- pr-description/index.js
+++ pr-description/index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  console.log(JSON.stringify(github.context.payload.pull_request, null, 2));
   const desc = github.context.payload.pull_request.description;
 
   if (desc == null || desc.replace(/\W/, "").length < 20) {
