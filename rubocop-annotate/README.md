e187137489df1f825f2be3e99132bff8fb73188f
Mohammad Satrio <wolfaeon@gmail.com>
2021-04-05 18:01:14 +0700

    Add rubocop-annotate code

---
+++ rubocop-annotate/README.md
@@ -0,0 +1,33 @@
+# Annotate RuboCop GitHub Action
+
+GitHub Action for creating annotations from RuboCop results. Forked from https://github.com/duderman/rubocop-annotate-action
+
+## Usage
+
+```yml
+name: Rubocop
+
+on: push
+
+jobs:
+  rubocop:
+    name: Rubocop
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: actions/setup-ruby@v1
+        with:
+          ruby-version: '2.7'
+      - run: gem install rubocop --no-doc
+      - run: rubocop --format progress --format json --out rubocop.json
+        id: rubocop
+      - uses: duderman/rubocop-annotate-action@v0.1.0
+        with:
+          path: rubocop.json
+          defaultLevel: warning
+          levels:
+            info: notice
+            refactor: warning
+            convention: warning
+        if: ${{ always() }}
+```
---
+++ rubocop-annotate/index.js
@@ -0,0 +1,34 @@
+const core = require("@actions/core");
+const command = require("@actions/core/lib/command")
+const github = require("@actions/github");
+const core = require("@actions/core");
+const path = require("path");
+const fs = require("fs");
+
+try {
+  const defaultAnnotationLevel = core.getInput("path");
+  const annotationLevel = core.getInput("levels");
+  const fullPath = path.resolve(core.getInput("path"));
+  const json = JSON.parse(fs.readFileSync(fullPath, "utf8"));
+
+  for (const file of json.files) {
+    for (const offense of file.offenses) {
+      const level = annotationLevel[offense.severity] || defaultAnnotationLevel;
+      const properties = {
+        file: file.path,
+        col: offense.location.column,
+        line: offense.location.line,
+      };
+      const message = `[${offense.cop_name}] ${offense.message}`;
+
+      command.issueCommand(level, properties, message);
+    }
+  }
+
+} catch (error) {
+  if (error.code === "ENOENT") {
+    core.setFailed(`File '${fullPath}' doesn't exist`)
+  } else {
+    core.setFailed(error.message);
+  }
+}
