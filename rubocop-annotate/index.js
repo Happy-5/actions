61a7ead5c67ca7a2a69e45916ac6cbdcf36af208
Mohammad Satrio <wolfaeon@gmail.com>
2021-04-06 00:10:57 +0700

    Prioritize severe offenses and grouped annotations

--- rubocop-annotate/index.js
+++ rubocop-annotate/index.js
@@ -4,14 +4,25 @@ const github = require("@actions/github");
 const path = require("path");
 const fs = require("fs");
 
+const levelPriority = { error: 0, warning: 10000, notice: 20000 };
+
+function higherLevel(level1, level2) {
+  if (level1 === "error" || (level1 === "warning" && level2 === "notice")) {
+    return level1;
+  } else {
+    return level2;
+  }
+}
+
 try {
   const annotationLevel = JSON.parse(core.getInput("levels"));
   const fullPath = path.resolve(core.getInput("path"));
   const json = JSON.parse(fs.readFileSync(fullPath, "utf8"));
+  const annotations = [];
 
   for (const file of json.files) {
 
-    const annotations = {};
+    const annotationMap = {};
 
     for (const offense of file.offenses) {
       const col = offense.location.column;
@@ -19,29 +30,26 @@ try {
       const properties = { file: file.path, line, col };
       const level = annotationLevel[offense.severity];
       const message = `[${offense.cop_name}] ${offense.message}`;
+      const key = `${file.path}:${line}`;
+      const annotation = annotationMap[key];
 
-      const key = `${file.path}:${line}`
-
-      if (annotations[key]) {
-        const annotation = annotations[key]
-
-        annotation.message = annotation.message + "\n" + message
-
-        if (annotation.level === "notice") {
-          annotation.level = level
-        } else if (annotation.level === "warning" && level !== "notice") {
-          annotation.level = level
-        }
-
+      if (annotation) {
+        annotation.size = annotation.size + 1;
+        annotation.message = annotation.message + "\n" + message;
+        annotation.level = higherLevel(annotation.level, level);
       } else {
-        annotations[key] = { level, properties, message }
+        annotationMap[key] = { size: 1, level, properties, message }
+        annotations.push(annotationMap[key]);
       }
     }
+  }
 
-    for (const annotationKey in annotations) {
-      const { level, properties, message } = annotations[annotationKey];
-      command.issueCommand(level, properties, message);
-    }
+  annotations.sort(function (a, b) {
+    return levelPriority[a.level] + a.size - levelPriority[b.level] - b.size;
+  });
+
+  for (const { level, properties, message } of annotations) {
+    command.issueCommand(level, properties, message);
   }
 
 } catch (error) {
